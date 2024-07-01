4ce7ff3addb56c17b534e7c04e5d6100196e2aa8
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-07-01 14:36:36 +0300

    Product, Slider, About Entites are created

---
+++ Foody.EntityLayer/Concrete/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.EntityLayer.Concrete
+{
+    public class Address
+    {
+        public int AddressID { get; set; }
+        public string AddresDetail { get; set; }
+        public string PhoneNumber { get; set; }
+         public string Email { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Foody.EntityLayer/Concrete/Slider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.EntityLayer.Concrete
+{
+    public class Slider
+    {
+        public int SliderID { get; set; }
+        public string SliderTitle { get; set; }
+        public string SliderImageUrl { get; set; }
+    }
+}
