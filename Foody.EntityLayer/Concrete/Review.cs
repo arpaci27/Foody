67ba628d2357dd738675e9bb4fc0eb0d538c8f23
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-07-03 00:04:16 +0300

    Entities Completed.

---
+++ Foody.EntityLayer/Concrete/Review.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.EntityLayer.Concrete
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerTitle { get; set; }
+            
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
