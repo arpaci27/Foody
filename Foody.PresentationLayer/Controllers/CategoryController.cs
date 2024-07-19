359a4aa7dce0d8b95bc822d76386fa84294254e8
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-07-19 16:45:38 +0300

    Enhanced architecture and added category listing

---
+++ Foody.PresentationLayer/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Foody.BusinessLayer.Abstract;
+using Foody.BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ICategoryService _categoryService;
+		public CategoryController(ICategoryService categoryService)
+		{
+			_categoryService = categoryService;
+		}
+
+		public IActionResult CategoryList()
+		{
+			var values = _categoryService.TGetAll();
+			return View(values);
+		}
+	}
+}
--- Foody.PresentationLayer/Program.cs
+++ Foody.PresentationLayer/Program.cs
@@ -1,3 +1,8 @@
+using Foody.BusinessLayer.Abstract;
+using Foody.BusinessLayer.Concrete;
+using Foody.DataAccessLayer.Abstract;
+using Foody.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
