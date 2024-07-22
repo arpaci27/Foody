5d514dfb6206a37bc08bf9d8f245567f7f3b44e7
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-07-22 17:36:53 +0300

    Enhanced DI setup and updated CategoryList view
    - In Program.cs, added references and configurations for dependency injection, including the registration of `FoodyContext`, `EfCategoryDal`, and `CategoryManager` to ensure the correct implementations are provided for interfaces.
    - Modified CategoryList.cshtml to fix an encoding issue and updated the model declaration to expect a `List` of `Category` entities. Removed placeholder table data and introduced dynamic generation of category information rows, including actions for delete and update. Added a new category addition button to improve UI.

--- Foody.PresentationLayer/Program.cs
+++ Foody.PresentationLayer/Program.cs
@@ -1,10 +1,15 @@
 using Foody.BusinessLayer.Abstract;
 using Foody.BusinessLayer.Concrete;
 using Foody.DataAccessLayer.Abstract;
+using Foody.DataAccessLayer.Concrete;
 using Foody.DataAccessLayer.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddDbContext<FoodyContext>();
+builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
