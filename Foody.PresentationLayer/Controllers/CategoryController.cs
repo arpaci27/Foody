e69b58494407ad50ea155e08e48ee9bb065bc73b
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-07-30 14:17:28 +0300

    Create Category operation completed.

--- Foody.PresentationLayer/Controllers/CategoryController.cs
+++ Foody.PresentationLayer/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Foody.BusinessLayer.Abstract;
 using Foody.BusinessLayer.Concrete;
+using Foody.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Foody.PresentationLayer.Controllers
@@ -16,6 +17,18 @@ namespace Foody.PresentationLayer.Controllers
 		{
 			var values = _categoryService.TGetAll();
 			return View(values);
+		}
+
+		[HttpGet]
+		public IActionResult AddCategory()
+        {
+            return View();
+        }
+		[HttpPost]
+		public IActionResult AddCategory(Category category)
+		{
+			_categoryService.TInsert(category);
+			return RedirectToAction("CategoryList");
 		}
 	}
 }
