8df5d859f97f1ac9f1b0a63e5c94cf106d90da4d
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-08-25 16:34:14 +0300

    CRUD operations of Products Completed. Sidebar Icons changed.

--- Foody.PresentationLayer/Controllers/ProductController.cs
+++ Foody.PresentationLayer/Controllers/ProductController.cs
@@ -1,15 +1,19 @@
 ﻿using Foody.BusinessLayer.Abstract;
+using Foody.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Foody.PresentationLayer.Controllers
 {
     public class ProductController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public ProductController(IProductService productService)
+        public ProductController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         public IActionResult ProductList()
@@ -21,6 +25,40 @@ namespace Foody.PresentationLayer.Controllers
         {
             var products = _productService.TProductListWithCateory();
             return View(products);
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            _productService.TDelete(id);
+            return RedirectToAction("ProductListWithCategory");
+        }
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            var values = _categoryService.TGetAll();
+            ViewBag.categories = new SelectList(values, "CategoryId", "CategoryName");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            _productService.TInsert(product);
+            return RedirectToAction("ProductListWithCategory");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var values = _categoryService.TGetAll();
+            ViewBag.categories = new SelectList(values, "CategoryId", "CategoryName");
+            var productValues = _productService.TGetById(id);
+            return View(productValues);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            _productService.TUpdate(product);
+            return RedirectToAction("ProductListWithCategory");
         }
     }
 }
